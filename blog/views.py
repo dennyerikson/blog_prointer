6f08c9c507f6ad328b3ac52e6767489cacf9cf5b
dennyerikson <dennyeriks00on@gmail.com>
2018-11-15 19:46:02 -0200

    add template base e models

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def post_list(request):
-    post = 'Hello World'
+    post = Post.objects.all()
+    
     context = {'post': post}
     return render(request, 'blog/post_list.html', context)
